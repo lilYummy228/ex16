7d2713b7db799c6a780e632f6e3186188352fd9a
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-05 09:55:49 +0500

    end

--- ex16/Program.cs
+++ ex16/Program.cs
@@ -10,7 +10,25 @@ namespace ex16
     {
         static void Main(string[] args)
         {
+            int minNumber = 2;
+            int maxNumber = 31;
+            int multiplier = 2;
+            int temporaryResult = 2;
+            int result = 0;
+            int degree = 1;
+            Random random = new Random();
+            int value = random.Next(minNumber, maxNumber);
+            Console.WriteLine($"Случайное число: {value}");
 
+            while (result < value)
+            {
+                result = multiplier * temporaryResult;
+                temporaryResult = result;
+                degree++;
+            }
+
+            Console.WriteLine($"Конечное число: {result}");
+            Console.WriteLine($"Для числа {value} минимальная степень двойки, превосходящее число {result}, равно {degree}");
         }
     }
 }
